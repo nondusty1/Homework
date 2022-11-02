6fb7edc470829b6eedf1fc42b8d2b6c0968761f9
Artem <artem.nodusty@gmail.com>
2022-11-02 18:06:59 +0300

    Added task3

---
+++ HW1/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Это число чётное");
+}
+else
+{
+    Console.WriteLine("Это число нечётное");
+}
