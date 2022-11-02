6d297bde34ba46d67084ce9e1a3ee361de3583af
Artem <artem.nodusty@gmail.com>
2022-11-02 23:58:23 +0300

    Added task3

---
+++ HW2/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int day = int.Parse(Console.ReadLine());
+
+if (day > 7)
+{
+    Console.WriteLine("В неделе всего 7 дней");
+}
+else if (day == 6 || day == 7)
+{
+    Console.WriteLine("Это выходной день");
+}
+else
+{
+    Console.WriteLine("Это будний день");
+}
