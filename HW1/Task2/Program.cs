4f747dcb08eb2bf3408e26f044bbd26feb214314
Artem <artem.nodusty@gmail.com>
2022-11-02 17:48:27 +0300

    Added task2

---
+++ HW1/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (num1 > num2)
+{
+    max = num1;
+}
+else
+{
+    max = num2;
+}
+if (max < num3)
+{
+    max = num3;
+}
+Console.WriteLine("Наибольшее из трех чисел - " + max);
