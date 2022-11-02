b95be2460d69634b72bf0621d55082ab697e743a
Artem <artem.nodusty@gmail.com>
2022-11-02 21:28:14 +0300

    Added task4

---
+++ HW1/Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = 0;
+
+while (num2 < num1)
+{
+    num2++;
+    if (num2 % 2 == 0)
+    {
+        Console.Write(num2 + " ");
+    }
+}
