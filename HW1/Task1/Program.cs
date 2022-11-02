10ac94a894fa80163eaa326bd88d0ffea8d0b9f0
Artem <artem.nodusty@gmail.com>
2022-11-02 17:16:50 +0300

    Added task1

---
+++ HW1/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("Большее число - " + num1);
+    Console.WriteLine("Меньшее число - " + num2);
+}
+else
+{
+    Console.WriteLine("Большее число - " + num2);
+    Console.WriteLine("Меньшее число - " + num1);
+}
