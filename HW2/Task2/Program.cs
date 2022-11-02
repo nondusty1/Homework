8a14d8ea586c4d37b6cbcf7aec447c2ccfe29265
Artem <artem.nodusty@gmail.com>
2022-11-02 22:33:40 +0300

    Added HW2 task2

---
+++ HW2/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+if (num >= 100)
+{
+    while (num > 999)
+    {
+      //num = num / 10;
+        num /= 10;
+    }
+    result = num % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Третья цифра отсутствует");
+}
