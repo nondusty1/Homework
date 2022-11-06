da4b8a8ef00583c3c062b8ae0dae5c0e564bbc30
Artem <artem.nodusty@gmail.com>
2022-11-06 21:23:11 +0300

    Added task1 in HW3

---
+++ HW3/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int mod = 0;
+int temp = 0;
+for (temp = num; num != 0; num /= 10)
+{
+    mod = num % 10;
+    sum = sum * 10 + mod;
+}
+if (temp == sum)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
