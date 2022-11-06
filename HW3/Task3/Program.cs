5102faeabae529c595ae62a4345fc64deda99371
Artem <artem.nodusty@gmail.com>
2022-11-07 01:25:02 +0300

    Added task3 in HW3

---
+++ HW3/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine ("Введите координату X первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координату Y первой точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координату Z первой точки: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координату X второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координату Y второй точки: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координату Z второй точки: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z2 - z1;
+
+double distance = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длинна отрезка {distance}");
