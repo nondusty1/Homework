7d51ad2b7b86059945c7aeeaf4523ededc63cf79
Artem <artem.nodusty@gmail.com>
2022-11-06 23:58:59 +0300

    Added task2 in HW3

---
+++ HW3/Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int count = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+for (i = 1; i <= count; i++)
+{
+    Console.Write("{1} в кубе это {2} \n", i, i, (i * i * i));
+}
