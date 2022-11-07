a6336d9c6ec7e0dfe79b2b0ac0951bad8f1564aa
Artem <artem.nodusty@gmail.com>
2022-11-07 23:35:21 +0300

    Added task1 in HW4

---
+++ HW4/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation(int b, int n)
+{
+    int result = 1;
+    for (int i = 0; i < n; i++)
+    {
+        result *= b;
+    }
+    return result;
+}
+Console.WriteLine(Exponentiation(b, n));
