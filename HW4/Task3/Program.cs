e870d6c6060327de23e6f330894fd195b4796eda
Artem <artem.nodusty@gmail.com>
2022-11-08 00:51:56 +0300

    Added task3 in HW4

---
+++ HW4/Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите количество элементов: ");
+int count = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[count];
+Console.WriteLine($"Массив из {count} элементов следующий: ");
+for (int i = 0; i < count; i++)
+{
+    array[i] = new Random().Next(1,100);
+    
+    Console.Write(array[i] + " ");    
+}
