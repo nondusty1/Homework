17ad4782bd39da8af6f1a0aeb0b2028b3357f600
Artem <artem.nodusty@gmail.com>
2022-11-08 00:19:04 +0300

    Added task2 in HW4

---
+++ HW4/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int LumpSum(int num)
+{
+    int temp = 0;
+    int sum = 0;
+    while (num > 0)
+    {
+        temp = num % 10;
+        sum += temp;
+        num /= 10;
+    }
+    return sum;
+}
+Console.WriteLine($"Сумма цифр в числе: {LumpSum(num)}");
