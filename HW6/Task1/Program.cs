4a4e06a49bd9cf991bae5aed187846b18afe5f4f
Artem <artem.nodusty@gmail.com>
2022-11-11 01:25:04 +0300

    added hw6

---
+++ HW6/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write($"Введите количество элементов в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+void FillArray(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введи {i + 1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int FindingPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;
+    }
+    return count;
+}
+FillArray(m);
+Console.WriteLine($"Введено чисел больше 0: {FindingPositive(array)} ");
---
+++ HW6/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите a:");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b:");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите c:");
+double c = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите d:");
+double d = Convert.ToDouble(Console.ReadLine());
+
+double X = (c - d) / (b - a);
+double Y = b * X + d;
+
+Console.WriteLine($"Точка пересечения двух прямых имеет координаты xy ({X};{Y})");
